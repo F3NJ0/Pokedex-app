e7d3b4d8249dca4a796c97722c6440ea25aaf4a0
F3NJ0 <58293913+F3NJ0@users.noreply.github.com>
2022-01-14 16:09:34 +0100

    Created array to hold pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-
+// Creating an array to hold pokemon objects
+let pokemonList = [];
+pokemonList[0] = {
+  name: 'Bulasaur',
+  height: 0.7,
+  types: ['grass', 'poison']
+};
+pokemonList[1] = {
+  name: 'Charmander',
+  height: 0.6,
+  types: ['fire']
+};
+pokemonList[2] = {
+  name: 'Squirtle',
+  height: 0.5,
+  types: ['water']
+};
