c4ba746c10880481f0bb343bab3d41c48bba8d84
F3NJ0 <58293913+F3NJ0@users.noreply.github.com>
2022-01-19 11:56:39 +0100

    Added IIFE for pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,41 @@
-// Creating an array to hold pokemon objects
-let pokemonList = [];
-pokemonList[0] = {
-  name: 'Bulasaur',
-  height: 0.7,
-  types: ['grass', 'poison']
-};
-pokemonList[1] = {
-  name: 'Charmander',
-  height: 0.6,
-  types: ['fire']
-};
-pokemonList[2] = {
-  name: 'Squirtle',
-  height: 0.5,
-  types: ['water']
-};
+// Creating and IIFE for pokemonList
+let pokemonRepository = (function(){
+  // Creating an array to hold pokemon objects
+  let pokemonList = [];
+  pokemonList[0] = {
+    name: 'Bulasaur',
+    height: 0.7,
+    types: ['grass', 'poison']
+  };
+  pokemonList[1] = {
+    name: 'Charmander',
+    height: 0.6,
+    types: ['fire']
+  };
+  pokemonList[2] = {
+    name: 'Squirtle',
+    height: 0.5,
+    types: ['water']
+  };
 
-// Printing the name and size of all pokemon onto page
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+// Creating a function to return name and size of all pokemon in a string
 function printArrayDetails(list){
   let output = "";
-  pokemonList.forEach(function(pokemon){
+  list.forEach(function(pokemon){
     output =  `${output} <br> ${pokemon.name} (height: ${pokemon.height})`;
     // Adding the comment "Wow, that's big" to pokemon that are taller than 0.6m
     if(pokemon.height > 0.6){
@@ -29,5 +44,7 @@ function printArrayDetails(list){
   });
   return output;
 }
-let str = printArrayDetails(pokemonList);
+
+// Printing the name and size of all pokemon onto page
+let str = printArrayDetails(pokemonRepository.getAll());
 document.getElementById("container").innerHTML = str;
